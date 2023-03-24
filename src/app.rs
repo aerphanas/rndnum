751a937a299bd67c0e0c4e8257f24d656f9172ae
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-24 16:07:36 +0700

    add button to increase random string length

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,38 @@
 use getrandom;
-use yew::{html, function_component, Html};
+use yew::{
+    html,
+    function_component,
+    Html,
+    use_state
+};
 
 #[function_component(App)]
 pub fn start() -> Html {
-    let number: String = (0..).map(|_| {
-        let mut buf = [0];
-        getrandom::getrandom(&mut buf).unwrap();
-        buf[0]
-    })
-    .filter(|&c| c <= 126 && c >= 33)
-    .map(|c| c as char)
-    .take(1000)
-    .collect();
-    
+    let counter = use_state(|| 0);
+    let number = |num| -> String {
+        (0..).map(|_| {
+            let mut buf = [0];
+            getrandom::getrandom(&mut buf).unwrap();
+            buf[0]
+        }).filter(|&c| c <= 126 && c >= 33)
+            .map(|c| c as char)
+            .take(num)
+            .collect()
+        };
+
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
     html!{
-        <p>{ format!("{}", number) }</p>
+        <>
+            <p> {"Current length : "} { *counter } </p>
+            <button {onclick}>{ "+1 Length" }</button>
+            <p>{ format!("{}", number(*counter)) }</p>
+        </>
     }
 }
